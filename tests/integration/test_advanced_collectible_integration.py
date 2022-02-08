86b891b805178308ab7a4f31f8628998b72af070
Mykyta <mykyta.onishchuck@code.berlin>
2022-02-08 12:59:27 +0100

    finished

--- tests/integration/test_advanced_collectible_integration.py
+++ tests/integration/test_advanced_collectible_integration.py
@@ -9,13 +9,13 @@ import time
 from scripts.advanced_collectible.deploy_and_create import deploy_and_create
 
 
-def test_can_create_advanced_collectible_integrations():
+def test_can_create_advanced_collectible_integration():
     # Deploy
     # Create NFT
     # Get a random breed
     # Arrang
-    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
-        pytest.skip("Only for local testing")
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for integration testing")
     # Act
     advanced_collectible, creation_tx = deploy_and_create()
     time.sleep(60)
